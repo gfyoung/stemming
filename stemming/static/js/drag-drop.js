c527f1f408b1e285896f7c00bb237a4b9201a908
gfyoung <gfyoung17@gmail.com>
2017-05-31 21:57:40 -0400

    Add drag-n-drop functionality

---
+++ stemming/static/js/drag-drop.js
@@ -0,0 +1,33 @@
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    // FileList object.
+    var files = evt.dataTransfer.files;
+    var reader = new FileReader();
+
+    reader.onload = function(event) {
+        document.getElementById("document").value = event.target.result;
+    };
+
+    if (!files[0].name.endsWith(".txt")) {
+        document.getElementById("document").value = "Sorry, only .txt files are accepted! Please try again.";
+    } else {
+        reader.readAsText(files[0], "UTF-8");
+    }
+}
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    // Explicitly show this is a copy.
+    evt.dataTransfer.dropEffect = "copy";
+}
+
+window.onload = function() {
+    var drop_zone = document.getElementById("document");
+    drop_zone.addEventListener("dragover", handleDragOver, false);
+    drop_zone.addEventListener("drop", handleFileSelect, false);
+};
+
