3d8dc098fc0b40cd43089aedc8c2aad82e27a8cb
gfyoung <gfyoung17@gmail.com>
2017-06-01 14:40:19 -0400

    Make documents GET-able
    We POST to store the document upon submission.
    However, the link at which we display the results
    is now visible via GET. This allows for easier
    navigation on the webpage.

--- stemming/webapp.py
+++ stemming/webapp.py
@@ -2,8 +2,10 @@
 Main web application for the user to provide documents for word counting.
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, url_for
 from .stemming import get_top_stems, stem_document
+
+import uuid
 
 app = Flask(__name__)
 
@@ -15,6 +17,22 @@ def index():
 
 @app.route("/submit", methods=["POST"])
 def submit():
+    doc_id = "@" + str(uuid.uuid4())
     document = request.form["document"]
+
+    response = redirect(url_for("display", **{"id": doc_id}))
+    response.set_cookie(doc_id, document)
+
+    return response
+
+
+@app.route("/display", methods=["GET"])
+def display():
+    doc_id = request.args.get("id")
+    document = request.cookies.get(doc_id)
+
+    if doc_id is None or document is None:
+        return redirect(url_for("index"))
+
     top_stems = get_top_stems(stem_document(document))
     return render_template("result.html", stems=top_stems)
--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -52,9 +52,55 @@ class TestSubmit(object):
         assert b"400 Bad Request" in rv.data
 
         rv = self.client.post(self.url, data=self.empty)
-        assert b"No stems found!" in rv.data
+        assert b"Redirecting" in rv.data
 
         rv = self.client.post(self.url, data=self.data)
+        assert b"Redirecting" in rv.data
+
+
+class TestDisplay(object):
+
+    def setup_class(self):
+        self.url = "/display"
+        self.client = app.test_client()
+        self.server_name = "test_server"
+
+        self.id = "123456789"
+        self.client.set_cookie(server_name=self.server_name, key=self.id,
+                               value="The cat jumped")
+
+        self.empty_id = "987654321"
+        self.client.set_cookie(server_name=self.server_name,
+                               key=self.empty_id, value="")
+
+    def get(self, doc_id):
+        """
+        Helper method for performing the GET request.
+
+        Parameters
+        ----------
+        doc_id : str
+            The document ID to request.
+
+        Returns
+        -------
+        get_response : flask.wrappers.Response
+            A Flask response object containing the data from the response.
+        """
+
+        return self.client.get(self.url + "?id=" + doc_id)
+
+    def test_get(self):
+        rv = self.get("")
+        assert b"Redirecting" in rv.data
+
+        rv = self.get("non-existent")
+        assert b"Redirecting" in rv.data
+
+        rv = self.get(self.empty_id)
+        assert b"No stems found" in rv.data
+
+        rv = self.get(self.id)
 
         assert b"the (1)" in rv.data
         assert b"cat (1)" in rv.data
@@ -62,3 +108,7 @@ class TestSubmit(object):
 
         assert b"Top Words" in rv.data
         assert b"Number in parentheses" in rv.data
+
+    def test_post(self):
+        rv = self.client.post(self.url, data=self.id)
+        assert b"405 Method Not Allowed" in rv.data
