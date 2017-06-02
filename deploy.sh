7758ce985c3267a1d268a09e047b5f306f57f6eb
gfyoung <gfyoung17@gmail.com>
2017-06-02 03:28:53 -0400

    Allow parameters in Heroku deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-git commit -a
-git push heroku master
+addAll='false'
+initDB='false'
 
-heroku run export FLASK_APP=stemming/webapp.py; flask initdb
-heroku restart
+while getopts 'Aih' flag; do
+    case "${flag}" in
+        h) echo 'deploy.sh - Helper for Heroku deployment'
+           echo ''
+           echo 'Usage: deploy.sh [options]'
+           echo ''
+           echo 'options:'
+           echo '-h: show brief help'
+           echo '-A: add all staged changes'
+           echo '-i: initialize database and restart'
+           exit 0
+           ;;
+        A) addAll='true' ;;
+        i) initDB='true' ;;
+     esac
+done
+
+if [ ${addAll} = 'true' ]; then
+    git add -A
+fi
+
+git commit -a
+git push -f heroku master
+
+if [ ${initDB} = 'true' ]; then
+    heroku run export FLASK_APP=stemming/webapp.py; flask initdb
+    heroku restart
+fi
